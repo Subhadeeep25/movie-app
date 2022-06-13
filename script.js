debb490b29a6604979ce7f03c2c2912db5882127
Subhadeep25 <84439620+Subhadeeep25@users.noreply.github.com>
2022-06-13 12:02:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+const api_url='https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=fe30e9b32d0066854a2b77f5458b580f&page=1'
+const img_path='https://image.tmdb.org/t/p/w1280'
+const search_url='https://api.themoviedb.org/3/search/movie?api_key=fe30e9b32d0066854a2b77f5458b580f&query="'
+const trending_url='https://api.themoviedb.org/3/trending/all/week?api_key=fe30e9b32d0066854a2b77f5458b580f'
+const form=document.getElementById('form');
+const main=document.getElementById('main');
+async function getMovies(url){
+    const response=await fetch(url)
+    const data=await response.json();
+    showMovies(data.results);
+    console.log(data.results);
+}
+
+
+function showMovies(movies){
+    movies.forEach((movie)=>{
+        const{title,poster_path,vote_average,overview  }=movie;
+        const movieEl=document.createElement('div');
+        movieEl.classList.add('movie');
+        movieEl.innerHTML=`
+        <img src="${img_path+poster_path}" alt="${title}">
+        <div class="movie-info">
+            <h3>${title}</h3>
+            <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+        </div>
+            <div class="overview">
+            <h3>Overview</h3>
+                ${overview}
+    </div>`;
+    main.appendChild(movieEl);
+    
+    })
+}
+
+function getClassByRate(vote){
+    if(vote>=8){
+        return 'green'
+    }else if(vote>=5){
+        return 'orange'
+    }
+    else
+        return 'red'
+}
+
+window.onloadstart=getMovies(trending_url);
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+   main.innerHTML="";
+    const searchTerm=search.value;
+    if(searchTerm && searchTerm!='')
+    {
+        getMovies(search_url+'');
+        getMovies(search_url+searchTerm);
+        search.value='';
+    }
+    else{
+        window.location.reload();
+    }
+})
